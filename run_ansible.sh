87192a604522bc0494d0baa5902bf942477a15dc
dojones1 <dojones1@users.noreply.github.com>
2021-01-10 18:27:14 +0000

    Basicly works

---
+++ run_ansible.sh
@@ -0,0 +1,4 @@
+#git clean -f
+#ansible-galaxy install -r roles/requirements.yml -p roles
+ansible-galaxy collection install community.kubernetes
+ansible-playbook --ask-become-pass -i ../inventory_localhost_mac/hosts.yml deploy_k3s_cluster.yml
