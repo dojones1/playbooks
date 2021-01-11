865f30426570f4ec5e23ab89e8851d704206bfc1
dojones1 <dojones1@users.noreply.github.com>
2021-01-11 22:46:03 +0000

    Add helloworld

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,4 +1,5 @@
 #git clean -f
 #ansible-galaxy install -r roles/requirements.yml -p roles
+pip3 install jmespath
 ansible-galaxy collection install community.kubernetes
 ansible-playbook --ask-become-pass -i ../inventory_localhost_mac/hosts.yml deploy_k3s_cluster.yml
